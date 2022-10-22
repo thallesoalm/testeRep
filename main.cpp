883dfee3dc9c056f33ce440320af6416d1c7a98b
thallesoalm <thalles.almeida@engenharia.ufjf.br>
2022-10-22 18:29:09 -0300

    Sera sorteado um numero entre 1 e 100, sera printado

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main(){
 int age;
+int num;
+
+srand(ntime(NULL));
 cout << "Hello, world!" << endl;
 cin >> age;
 
 cout << "Sua idade eh: << age << endl;
 
+num = rand()%100+1;
+cout << "O numero sorteado eh: << num";
 return 0;
 }
